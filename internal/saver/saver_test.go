648fecf28dcfc2104f7f76e7afc7cefdce0011fe
Anton B <bukhtiyarov.anton@gmail.com>
2021-08-09 20:45:33 +0700

    add lecture 5 homework: - saver interface with mocks and tests

---
+++ internal/saver/saver_test.go
@@ -0,0 +1,48 @@
+package saver_test
+
+import (
+	"time"
+
+	"github.com/golang/mock/gomock"
+	. "github.com/onsi/ginkgo"
+
+	//. "github.com/onsi/gomega"
+
+	"github.com/ozoncp/ocp-vacancy-api/internal/mocks"
+	"github.com/ozoncp/ocp-vacancy-api/internal/models"
+	"github.com/ozoncp/ocp-vacancy-api/internal/saver"
+)
+
+var _ = Describe("Saver", func() {
+	var (
+		ctrl *gomock.Controller
+
+		mockFlusher *mocks.MockFlusher
+
+		s saver.Saver
+	)
+
+	BeforeEach(func() {
+		ctrl = gomock.NewController(GinkgoT())
+
+		mockFlusher = mocks.NewMockFlusher(ctrl)
+
+		s = saver.NewSaver(10, mockFlusher, 10*time.Millisecond)
+	})
+
+	AfterEach(func() {
+		ctrl.Finish()
+	})
+
+	Context("When saving a vacancy", func() {
+		It("should call Flush", func() {
+			mockFlusher.EXPECT().
+				Flush(gomock.Any()).
+				Times(1)
+
+			s.Save(models.Vacancy{})
+			s.Close()
+			time.Sleep(50 * time.Millisecond) // sleep for 50ms to guarantee s.flush call
+		})
+	})
+})
